b51669c91e60498d0d4fc84b2b6a3bc7ef61ee67
JimmyGAR <jimmy.garnier1110@gmail.com>
2026-03-31 08:54:40 +0200

    Enumérations PHP

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -10,7 +10,7 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status,
     ) {
         
     }
@@ -31,7 +31,7 @@ class Starship
         return $this->captain;
     }
 
-    public function getStatus(): string {
+    public function getStatus(): StarshipStatusEnum {
         return $this->status;
     }
 }
